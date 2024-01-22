c557ff5c128fc2766850079772477f24208f8a93
Shailsharma2604 <shailsharma020604@gmail.com>
2024-01-22 09:59:45 +0530

    1

--- class/Java/Boxclass.java
+++ class/Java/Boxclass.java
@@ -7,16 +7,38 @@ public class Boxclass{
     double width;
     double height;
     double depth;
+    double volume(){
+        // System.out.println("The volume of box is : "+width*depth*height);
+        return width*depth*height;
+    }
+    void setDim(double w,double h,double d){
+        width=w;
+        height=h;
+        depth=d;
+    }
 }
 // public class vol
 class Main{
     public static void main(String[] args) {
         Boxclass b1 = new Boxclass();
-        double vol;
-        b1.width=10;
-        b1.depth=9;
-        b1.height=11;
-        vol=b1.depth*b1.height*b1.width;
-        System.out.println("The volume of box is :"+vol);
+        // double vol;
+        // b1.width=10;
+        // b1.depth=9;
+        // b1.height=11;
+        b1.setDim(1, 5, 8);
+        double vol=b1.volume();
+        System.out.println("The Volume of box 1 : "+vol);
+
+
+
+        Boxclass b2 = new Boxclass();
+        // double vol;
+        // b2.width=1;
+        // b2.depth=9;
+        // b2.height=1151;
+        b2.setDim(7, 54, 54);
+        vol=b2.volume();
+        System.out.println("The Volume of box 2 : "+vol);
+        
     }
 }

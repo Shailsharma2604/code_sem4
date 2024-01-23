baa564bee49de0540fc51471a4f520b1eb556eec
Shailsharma2604 <shailsharma020604@gmail.com>
2024-01-23 11:02:17 +0530

    1

---
+++ class/flask/class_3.py
@@ -0,0 +1,21 @@
+from flask import Flask,redirect,url_for
+
+app=Flask(__name__)
+
+@app.route("/admin")
+def hello_admin():
+    return "<h1>Hello,World!<h1/>"
+
+@app.route('/guest/<guest>')
+def hello_guest(guest):
+    return 'hello %s as guest' %guest
+
+@app.route('/user/<name>')
+def hello_user(name):
+    if name=='admin':
+        return redirect(url_for('hello_admin'))
+    else:
+        return redirect(url_for('hello_guest',guest=name))
+
+if __name__=="__main__":
+    app.run(debug=True)

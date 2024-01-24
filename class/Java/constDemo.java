3cbd3def89c5c2f3bf30f45d7e11913c9da063a7
Shailsharma2604 <shailsharma020604@gmail.com>
2024-01-24 14:43:33 +0530

    1

--- class/Java/constDemo.java
+++ class/Java/constDemo.java
@@ -1,8 +1,36 @@
+// No return type
+// Self calling; Called at the time of object creation
+// Name of constructor is same as class name
 
-class demop
 
-public class constDemo {
+
+
+class Box_ {
+    double width;
+    double height;
+    double depth;
+    double volume(){
+        return width*depth*height;
+    }
+    Box_(int w,int h,int d){
+        System.out.println("const box");
+        this.depth=d;
+        this.height=h;
+        this.width=w;
+    }
+    Box_(int val){
+        width=height=depth=val;
+    }
+}
+
+
+
+public class constDemo{
     public static void main(String[] args) {
-        
+        Box_ box1 = new Box_(10,11,12);
+        System.out.println(box1.volume());
+        Box_ box2 = new Box_(2);
+        System.out.println(box2.volume());
+
     }
 }

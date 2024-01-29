61bbabafa054e73954cf39d1e6a2224fa3426794
Shailsharma2604 <shailsharma020604@gmail.com>
2024-01-29 09:39:47 +0530

    1

---
+++ class/Java/InDemo.java
@@ -0,0 +1,5 @@
+public class InDemo {
+    public static void main(String[] args) {
+        
+    }
+}
--- class/Java/thisKeyword.java
+++ class/Java/thisKeyword.java
@@ -1,8 +1,8 @@
 
 class _box_{
     int num;
-    _box_(int n){
-        num=n;
+    _box_(int num){
+        this.num=num;
         System.out.println("the value of num : "+num);
     }
 }
@@ -10,5 +10,8 @@ class _box_{
 public class thisKeyword {
     public static void main(String[] args) {
         _box_ obj=new _box_(10);
+        System.out.println("obj_1 : "+obj.num);
+        
+        
     }
 }

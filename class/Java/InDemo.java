3cca452b357d275b8e6f2d71459f2014de1630eb
Shailsharma2604 <shailsharma020604@gmail.com>
2024-01-29 10:08:52 +0530

    1

--- class/Java/InDemo.java
+++ class/Java/InDemo.java
@@ -1,5 +1,39 @@
+
+class A {
+    int i;
+    int j;
+
+    void showij() {
+        System.out.println("i : " + i);
+        System.out.println("j : " + j);
+    }
+}
+
+class B extends A{
+    int k;
+
+    void showk() {
+        System.out.println("k: " + k);
+    }
+
+    void sum() {
+        System.out.println("i+j+k : "+(i+j+k));
+    }
+}
 public class InDemo {
     public static void main(String[] args) {
-        
+        A superobj = new A();
+        B subobj = new B();
+
+        superobj.i = 10;
+        superobj.j = 12;
+        superobj.showij();
+
+        subobj.i=7;
+        subobj.j=8;
+        subobj.k=9;
+        subobj.showij();
+        subobj.showk();
+        subobj.sum();
     }
 }

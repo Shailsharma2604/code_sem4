551c82486d26d5d449a37d9bf01a95f5aa3e5beb
Shailsharma2604 <shailsharma020604@gmail.com>
2024-01-30 09:07:49 +0530

    1

--- practice/Java/day_4.java
+++ practice/Java/day_4.java
@@ -1,5 +1,7 @@
 public class day_4 {
     public static void main(String[] args) {
-        
+        // String name = new String("hello");
+        String name="hello";
+        System.out.println(name);
     }
 }

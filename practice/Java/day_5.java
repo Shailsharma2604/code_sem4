b340eb0d35b821199acc73c82dde7553a49b770a
Shailsharma2604 <shailsharma020604@gmail.com>
2024-02-12 00:46:01 +0530

    1

--- practice/Java/day_5.java
+++ practice/Java/day_5.java
@@ -1,5 +1,11 @@
 public class day_5 {
     public static void main(String[] args) {
-        
+        int[] marks =new int[5];
+        marks[0]=100;
+        marks[1]=80;
+        marks[2]=60;
+        marks[3]=40;
+        marks[4]=20;
+        System.out.println(marks[4]);
     }
 }

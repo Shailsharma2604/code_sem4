b119b40b0baa2c6b2716b4ce790f3bd0337e3695
Shailsharma2604 <shailsharma020604@gmail.com>
2024-02-29 01:12:18 +0530

    1

--- practice/Java/day_6.java
+++ practice/Java/day_6.java
@@ -1,5 +1,26 @@
 public class day_6 {
+    int logic(int x,int y){
+        int z;
+        if(x>y){
+            z=x+y;
+        }
+        else{
+            z=(x+y)*5;
+        }
+        return z;
+    }
     public static void main(String[] args) {
-        
+        int a=5;
+        int b=7;
+        int c;
+        // day_6 obj= new day_6();
+        // c=obj.logic(a, b);
+        int a1=2;
+        int b1=1;
+        int c1;
+        // c1=obj.logic(a1, b1);
+        System.out.println(c);
+        System.out.println(c1);
     }
 }
+ 

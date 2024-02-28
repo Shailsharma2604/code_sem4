339bf6ab12eff476a4e31561bac7917b6d32cdc0
Shailsharma2604 <shailsharma020604@gmail.com>
2024-02-28 11:21:39 +0530

    1

--- class/flask/class_9.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-app=Flask(__name__)
-app.debug=True
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
-db=SQLAlchemy(app)
-if __name__=='__main__':
-    app.run()

98369de243e20eb007bc1e82826661ed97ac3fad
Shailsharma2604 <shailsharma020604@gmail.com>
2024-02-29 13:53:38 +0530

    1

--- class/Java/Keyword_Used_Exception_Handling.java
+++ class/Java/Keyword_Used_Exception_Handling.java
@@ -18,15 +18,40 @@ public class Keyword_Used_Exception_Handling {
         // catch(ArithmeticException A){
         //     System.out.println(A);
         // }
-        try
-        {
-            
-            int a=5/0;
-        }
-        finally
-        {
-            System.out.println("Hello");
-        }
-        System.out.println("Bye");
+        // try
+        // {
+
+        //     int a=5/0;
+        // }
+        // finally
+        // {
+        //     System.out.println("Hello");
+        // }
+        // System.out.println("Bye");
     }
 }
+
+
+// String Buffer()
+// String Buffer(int size)
+// String Buffer(String str)
+// String Buffer(charSquence chars)
+
+// Default constructor 
+
+// StringBuffer str=new StringBuffer()   capacity=21
+// StringBuffer str=new StringBuffer(50)   capacity=50
+// StringBuffer str=new StringBuffer("Hello class")   capacity=21
+
+
+// 1. length() and capacity()
+// 2. ensureCapacity()
+// 3. setLength()
+// 4. charAt() and setCharAt()
+// 5. getchars()
+// 6. append
+// 7. insert()
+// 8. reverse()
+// 9. delete() and deleteCharAt()
+// 10. replace()
+// 11. subString()

7f283ce222c998b54b0b37511b24c0ec2d942648
Shailsharma2604 <shailsharma020604@gmail.com>
2024-03-13 21:45:08 +0530

    1

--- practice/D.S.A/Question_1.py
+++ practice/D.S.A/Question_1.py
@@ -1,13 +1,27 @@
 
 ######## 1 ########
 
-a=input()
+# a=input()
 
-if a >= 'A' and a<= 'Z':
-    print("1")
+# if a >= 'A' and a<= 'Z':
+#     print("1")
 
-elif a >= 'a' and a<= 'z':
-    print("0")
+# elif a >= 'a' and a<= 'z':
+#     print("0")
 
-else:
-    print("-1")
+# else:
+#     print("-1")
+
+######## 2 ########
+
+from typing import *
+
+def dataTypes(type: str):
+    if type=="Long":
+        return 8
+    elif type=="Double":
+        return 8
+    elif type=="Character":
+        return 1
+    else:
+        return 4

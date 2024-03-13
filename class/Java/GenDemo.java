8bf983be1679deaf6c75084c53a6bdffb4749d1e
Shailsharma2604 <shailsharma020604@gmail.com>
2024-03-13 14:10:32 +0530

    1

--- class/Java/GenDemo.java
+++ class/Java/GenDemo.java
@@ -1,19 +1,32 @@
+// Syntax : class name<object type>
+// Only take wrapper classes
 
-class Gen<T>{
-    T ob;
-    Gen(T o){
-        ob = o;
+
+class gen<T> {
+    T obj;
+    gen(T name) {              // Type t with variable name
+        obj = name;
     }
-    T getdata(){
-        return ob;
+
+    T getData() {
+        return obj;
     }
-    void showType(){
-        System.out.println("Type of ob is " + ob.getClass().getName());
+
+    void Showtype() {
+        System.out.println("Type is: "+ obj.getClass().getName());
     }
 }
 
-public class GenDemo {
+public class GenDemo{
     public static void main(String[] args) {
-        Gen ob;
+        gen<Integer> iob = new gen<Integer>(9);                  // Object Creation ; Integer is wrapper classes
+        iob.Showtype();
+        System.out.println(iob.getData());
+
+        System.out.println();
+        
+        gen<String> sob = new gen<String>("String");
+        sob.Showtype();
+        System.out.println(sob.getData());
     }
 }
---
+++ class/Java/NonGenDemo.java
@@ -0,0 +1,5 @@
+public class NonGenDemo {
+    public static void main(String[] args) {
+        
+    }
+}

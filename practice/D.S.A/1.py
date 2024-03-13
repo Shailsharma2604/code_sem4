a0f1e7e255483375b4f7ceeea61a38c54cbdb9aa
Shailsharma2604 <shailsharma020604@gmail.com>
2024-03-13 21:30:15 +0530

    1

--- practice/D.S.A/1.py
+++ practice/D.S.A/1.py
@@ -52,10 +52,12 @@ b=True
 c="Harry"
 d=None
 print(a)
-print(b)
-print(a+b)
-
 print("The type of a is", type(a))
+print(b)
 print("The type of b is", type(b))
+print(c)
 print("The type of c is", type(c))
+print(d)
 print("The type of d is", type(d))
+print(a+b)
+print("The type of d is", type(a+b))

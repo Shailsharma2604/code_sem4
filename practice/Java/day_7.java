5b1ad932faf302cc76543bf12c97607a49d5f33c
Shailsharma2604 <shailsharma020604@gmail.com>
2024-03-14 01:02:58 +0530

    1

---
+++ practice/Java/day_7.java
@@ -0,0 +1,26 @@
+public class day_7 {
+    // static int sum(int a,int b){
+    //     return a+b;
+    // }
+    // static int sum(int a,int b,int c){
+    //     return a+b+c;
+    // }
+    // static int sum(int a,int b,int c,int d){
+    //     return a+b+c+d;
+    // }
+    static int sum(int ...arr){
+        // Available as int [] arr;
+        int result=0;
+        for(int a:arr){
+            return += a;
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        System.out.println("Welcome to Varargs Tutorial"); 
+        System.out.println("The sum of 4,5 is :- "+sum(4,5));
+        System.out.println("The sum of 4,5,6 is :- "+sum(4,5,5));
+        System.out.println("The sum of 4,5,6,7 is :- "+sum(4,5,6,7));
+        System.out.println("The sum of 4,5,6,7,8 is :- "+sum(4,5,6,7,8));
+    }
+}

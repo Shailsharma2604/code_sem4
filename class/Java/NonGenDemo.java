d9e6a62ec596c1f9a4faaebb43bfa55c6b4c932f
Shailsharma2604 <shailsharma020604@gmail.com>
2024-03-13 14:23:09 +0530

    1

--- class/Java/NonGenDemo.java
+++ class/Java/NonGenDemo.java
@@ -1,5 +1,26 @@
+
+class NonGen{
+    Object ob;
+    NonGen(Object o){
+        ob = o;
+    }
+
+    Object getData(){
+        return ob;
+    }
+
+    void Showtype(){
+        System.out.println("Type is : "+ ob.getClass().getName());
+    }
+}
+
 public class NonGenDemo {
     public static void main(String[] args) {
-        
+       NonGen iob;
+       iob = new NonGen(78);
+       iob.Showtype();
+
+       int v= (Integer) iob.getData();
+       System.out.println("valve : "+v);
     }
 }

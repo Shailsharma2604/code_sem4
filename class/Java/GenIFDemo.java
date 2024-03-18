471fb471e2ac14e70dd6e2c61bb58e9ba3eba5f8
Shailsharma2604 <shailsharma020604@gmail.com>
2024-03-18 09:50:37 +0530

    1

--- class/Java/GenIFDemo.java
+++ class/Java/GenIFDemo.java
@@ -15,15 +15,22 @@ class MyClass<T extends Comparable<T>>implements MinMax<T>{
 
     public T min(){
         T v=vals[0];
-        for (int i = 1; i < vals.length; i++) {
+        for (int i = 0; i < vals.length; i++) {
             if(vals[i].compareTo(v)<0){
                 v=vals[i];
             }
         }
+        return v;
     }
 
     public T max(){
-
+        T v=vals[0];
+        for (int i = 0; i < vals.length; i++) {
+            if(vals[i].compareTo(v)>0){
+                v=vals[i];
+            }
+        }
+        return v;
     }
 }
 
@@ -33,6 +40,14 @@ class MyClass<T extends Comparable<T>>implements MinMax<T>{
 
 public class GenIFDemo {
     public static void main(String[] args) {
-        
+        Integer inums[]={4,2,21,40,12};
+        MyClass<Integer> iob= new MyClass<>(inums);
+        System.out.println("Min :- "+iob.min());
+        System.out.println("Max :- "+iob.max());
+
+        Character chars[]={'b','r','o','i','e'};
+        MyClass<Character> cob= new MyClass<>(chars);
+        System.out.println("Min :- "+cob.min());
+        System.out.println("Max :- "+cob.max());
     }
 }

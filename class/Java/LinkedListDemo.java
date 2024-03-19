02b80e0c40988f5240f8032f5ca5dec0f21688ce
Shailsharma2604 <shailsharma020604@gmail.com>
2024-03-19 10:09:12 +0530

    1

--- class/Java/LinkedListDemo.java
+++ class/Java/LinkedListDemo.java
@@ -1,22 +1,42 @@
+import java.util.LinkedList;
+
 public class LinkedListDemo {
     public static void main(String[] args) {
-        al.add(1);
-        al.add(2);
-        al.add(3);
-        al.add(4);
-        al.add(5);
+        LinkedList<String> a1 = new LinkedList<String>();
 
-        System.out.println("Content of al :- "+al);
-
-        Integer ia[]=new Integer[al.size()];
-        ia=al.toArray(ia);
-
-        int sum=0;
-        for(int i:ia)
-            sum+=i;
-            
-        System.out.println("Content of array :- "+sum);
+        System.out.println("Size: " + a1.size());
         
-        System.out.println("Content of array :- "+ia);
+        a1.add("A");
+        a1.add("B");
+        a1.add("C");
+        a1.add("D");
+        a1.add("E");
+        
+        a1.add(1, "A2");
+        
+        System.out.println("Size: " + a1.size());
+        System.out.println("Contents: " + a1);
+        
+        a1.remove("D");         // Removing using element
+        a1.remove(0);           // Removing using index
+        
+        System.out.println("Size: " + a1.size());
+        System.out.println("Contents: " + a1);
+        
+        a1.addFirst("Welcome");
+        System.out.println("Size: " + a1.size());
+        System.out.println("Contents: " + a1);
+        
+        a1.addLast("Last");
+        System.out.println("Size: " + a1.size());
+        System.out.println("Contents: " + a1);
+        
+        a1.removeFirst();
+        a1.removeLast();
+        System.out.println("Contents: " + a1);
+        
+        String value = a1.get(0);
+        a1.set(0, value+" changed");
+        System.out.println("Contents: " + a1);
     }
 }

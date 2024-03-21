78df75c53e34f756e3c8e4b99655df4db3a72cae
Shailsharma2604 <shailsharma020604@gmail.com>
2024-03-21 14:04:27 +0530

    1

--- class/Java/StackDemo.java
+++ class/Java/StackDemo.java
@@ -4,16 +4,31 @@ import java.util.*;
 public class StackDemo {
 
     static void showPush(Stack<Integer>st,int a){
-
+        // To push elemnts in stack
+        st.push(a);
+        // To show the element
+        System.out.println("push("+a+")");
+        // To show the elements of stack
+        System.out.println("stack :- "+st);
     }
-    static void showPop(){
-
+    static void showPop(Stack<Integer>st){
+        // To pop elemnts in stack
+        Integer val=st.pop();
+        // To show the element
+        System.out.println("pop :-"+val);
+        // To show the elements of stack
+        System.out.println("stack :- "+st);
     }
     public static void main(String[] args) {
         Stack<Integer>st=new Stack<Integer>();
-        st.push(21);
-        st.push(22);
-        st.push(23);
-        st.push(24);
+        showPush(st,21);
+        showPush(st,22);
+        showPush(st,23);
+        showPush(st,24);
+        System.out.println("content of stack :- "+st);
+        showPop(st);
+        showPop(st);
+        showPop(st);
+        showPop(st);
     }
 }

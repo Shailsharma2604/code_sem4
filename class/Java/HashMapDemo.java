385ac85d2e8ec6194065c49cb1c9ff8e880ec23c
Shailsharma2604 <shailsharma020604@gmail.com>
2024-04-01 10:13:57 +0530

    1

--- class/Java/HashMapDemo.java
+++ class/Java/HashMapDemo.java
@@ -12,11 +12,23 @@ public class HashMapDemo {
         System.out.println(hMap);
 
         // to get a set of the entries
-
         Set<Map.Entry<String,Double>>hSet=hMap.entrySet();
 
         // display the set
         System.out.println("Set :-"+hSet);
-        
+
+        for(Map.Entry<String,Double>entry : hSet) {
+            System.out.println(entry.getKey()+":");
+            System.out.println(entry.getValue());
+        }
+
+        // deposite INR 1000 to john's account
+        double acc=hMap.get("john");
+        System.out.println(acc);
+
+        hMap.put("john", acc + 1000);
+        System.out.println("Updated Hash Map :-"+hMap);
+        System.out.println("John's new balanace :-");
+        System.out.println(hMap.get("John"));
     }
 }

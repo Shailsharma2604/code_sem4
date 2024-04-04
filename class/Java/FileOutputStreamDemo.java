57ad89d14694376410da153b3d417cc5f4e667f6
Shailsharma2604 <shailsharma020604@gmail.com>
2024-04-04 14:32:25 +0530

    1

---
+++ class/Java/FileOutputStreamDemo.java
@@ -0,0 +1,5 @@
+public class FileOutputStreamDemo {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ class/Java/FileinputStreamDemo3.java
@@ -0,0 +1,7 @@
+
+
+public class FileinputStreamDemo3 {
+    public static void main(String[] args) {
+        
+    }
+}

79e86c415c9933ec0d0559517779bccee6c5a0d8
Shailsharma2604 <shailsharma020604@gmail.com>
2024-04-04 15:03:58 +0530

    1

--- class/Java/FileOutputStreamDemo.java
+++ class/Java/FileOutputStreamDemo.java
@@ -3,21 +3,21 @@ import java.io.*;
 
 public class FileOutputStreamDemo {
     public static void main(String[] args) {
-        String source ="This is my files";
-        + "This is my files";
+        String source ="This is my files"
+        + "This is my files"
         + "This is my files";
 
         byte buf[] = source.getBytes();
 
 
-        FileOutputStream f0 = null;
-        FileOutputStream f1 = null;
-        FileOutputStream f2 = null;
-        
+        // FileOutputStream f0 = null;
+        // FileOutputStream f1 = null;
+        // FileOutputStream f2 = null;
+
         try {
-            f0 = new FileOutputStream("file1.txt");
-            f1 = new FileOutputStream("file2.txt");
-            f2 = new FileOutputStream("file3.txt");
+            FileOutputStream f0 = new FileOutputStream("FileOutputfile1.txt");
+            FileOutputStream f1 = new FileOutputStream("FileOutputfile2.txt");
+            FileOutputStream f2 = new FileOutputStream("FileOutputfile3.txt");
 
             for (int i = 0; i < buf.length; i += 2) {
                 f0.write(buf[i]);
@@ -26,32 +26,36 @@ public class FileOutputStreamDemo {
             f1.write(buf);
 
             f2.write(buf, buf.length - buf.length/4, buf.length/4);
+            
+            f0.close();
+            f1.close();
+            f2.close();
         } catch (IOException e) {
             System.out.println("An I/O Error Occurred");
         } finally {
-            try {
-                if (f0 != null) {
-                    f0.close();
-                }
-            } catch (IOException e) {
-                System.out.println("Error Closing file1.txt");
-            }
+            // try {
+            //     if (f0 != null) {
+            //         f0.close();
+            //     }
+            // } catch (IOException e) {
+            //     System.out.println("Error Closing file1.txt");
+            // }
 
-            try {
-                if (f1 != null) {
-                    f1.close();
-                }
-            } catch (IOException e) {
-                System.out.println("Error Closing file2.txt");
-            }
+            // try {
+            //     if (f1 != null) {
+            //         f1.close();
+            //     }
+            // } catch (IOException e) {
+            //     System.out.println("Error Closing file2.txt");
+            // }
 
-            try {
-                if (f2 != null) {
-                    f2.close();
-                }
-            } catch (IOException e) {
-                System.out.println("Error Closing file3.txt");
-            }
+            // try {
+            //     if (f2 != null) {
+            //         f2.close();
+            //     }
+            // } catch (IOException e) {
+            //     System.out.println("Error Closing file3.txt");
+            // }
         }
 
     }

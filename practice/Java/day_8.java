d12c546e52be678e46a873f1c73b66746a8bc9b4
Shailsharma2604 <shailsharma020604@gmail.com>
2024-05-28 17:55:18 +0530

    1

--- practice/Java/day_8.java
+++ practice/Java/day_8.java
@@ -1,26 +1,57 @@
+
+import java.util.Arrays;
+
 public class day_8 {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        String name="shail";
-        int age=19;
-        String friend="nitish";
-        String neighbour=friend;
+        // System.out.println("Hello world!");
+        // String name="shail";
+        // int age=19;
+        // String friend="nitish";
+        // String neighbour=friend;
         
-        byte age1= 20;
-        int phone = 897541532;
-        long phone2=753333333336L;
-        float pi=3.14F;
-        char letter='@';
-        boolean isAdult=false;
+        // byte age1= 20;
+        // int phone = 897541532;
+        // long phone2=753333333336L;
+        // float pi=3.14F;
+        // char letter='@';
+        // boolean isAdult=false;
 
 
-        String name1= "Aman";
-        System.out.println("name1 : "+name1.length());
-        String name3 = new String("Apu");
-        String Friend = new String("Akku");
+        // String name1= "Aman";
+        // System.out.println("name1 : "+name1.length());
+        // String name3 = new String("Apu");
+        // String Friend = new String("Akku");
 
+
+        // String name1 = "Aman";
+        // String name2 = "Akku";
+        // String name3 = name1 + name2;
+        // System.out.println(name3);
+
+        // String name="Aman";
+        // System.out.println(name.charAt(1));
+        // System.out.println(name.length());
+        // System.out.println(name.replace('a','b'));
+        // String name1 = "Aman and Akku";
+        // System.out.println(name1.substring(0, 4));
+
+
+        // int[] marks = new int[3];
+        // marks[0]=97;
+        // marks[1]=98;
+        // marks[2]=95;
+        // System.out.println(marks);
+        // System.out.println(marks[0]);
+        // System.out.println(marks[1]);
+        // System.out.println(marks[2]);
+
+        // System.out.println(marks.length);
+
+        // Arrays.sort(marks);
+        // System.out.println(marks[0]);
+        // System.out.println(marks[1]);
+        // System.out.println(marks[2]);
 
         
-
     }
 }

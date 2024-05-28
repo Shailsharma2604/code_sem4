f790c4a1d821ffeebb51efffa667c08983d21477
Shailsharma2604 <shailsharma020604@gmail.com>
2024-05-28 16:15:08 +0530

    1

--- practice/Java/day_8.java
+++ practice/Java/day_8.java
@@ -1,5 +1,26 @@
 public class day_8 {
     public static void main(String[] args) {
+        System.out.println("Hello world!");
+        String name="shail";
+        int age=19;
+        String friend="nitish";
+        String neighbour=friend;
         
+        byte age1= 20;
+        int phone = 897541532;
+        long phone2=753333333336L;
+        float pi=3.14F;
+        char letter='@';
+        boolean isAdult=false;
+
+
+        String name1= "Aman";
+        System.out.println("name1 : "+name1.length());
+        String name3 = new String("Apu");
+        String Friend = new String("Akku");
+
+
+        
+
     }
 }
